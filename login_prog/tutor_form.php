8863480d8c30cf89d9442176bf987c0f9cb3af75
Ale-ch2 <alessandrochiarugi12@gmail.com>
2025-06-30 12:38:57 +0200

    version 1.0

---
+++ login_prog/tutor_form.php
@@ -0,0 +1,19 @@
+<?php
+$nome_utente = $_POST['nome_utente'];
+$pwd = $_POST['pwd'];
+
+session_start();
+include "connection.php";
+
+$sql = "SELECT nome_utente, pwd FROM utenti WHERE nome_utente = '$nome_utente'  AND pwd =  '$pwd'"; // chiedere riguardo a questa linea e a proposito di sanificare i dati
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $_SESSION["nome_utente"] = $nome_utente;
+    header('Location: ../html/successfull.php');
+    exit(); //Perchè exit in questo modo? cos'è?
+} else {
+    header('Location: ../html/unsuccessfull.htm');
+    exit();
+}
